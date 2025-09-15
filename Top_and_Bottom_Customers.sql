58535226d7103fdbc4d442b1192025236597e64d
wadie tawfik <wadea28120055@gmail.com>
2025-09-15 18:46:01 +0300

    Add files via upload

---
+++ Top_and_Bottom_Customers.sql
@@ -0,0 +1,20 @@
+﻿SELECT * 
+FROM sheetcustomers;
+
+SELECT top 3
+    [Customer Id],
+    [full name],
+    SUM ([Total Amount]) AS "total spend"
+FROM sheetcustomers
+GROUP BY [Customer ID], [full name]
+ORDER BY "total spend" ASC;
+
+
+SELECT TOP 3
+    [Customer Id],
+    [full name],
+    SUM([Total Amount]) AS TS
+FROM sheetcustomers
+GROUP BY [Customer Id], [full name]
+ORDER BY TS DESC;
+
